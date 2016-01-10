e6765b842a9d089cf39af32add1d5debe9033cf2
Wyatt Thornbury <w.m.thornbury@hotmail.com>
2016-01-10 13:34:30 -0800

    Added: BaseCharacter class and Stats class

---
+++ CodeNameKidsNextDoor/Assets/Scripts/BaseCharacter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using code.stats;
+
+namespace code.baseCharacter
+{
+    public class BaseCharacter
+    {
+        #region Vars
+        Stats characterStats = null;
+        #endregion
+
+        #region Methods
+        public BaseCharacter(int hp = 2, int def = 2, int str = 2, int agl = 2, int intellect = 2, int wis = 2)
+        {
+            characterStats = new Stats(hp, def, str, agl, intellect, wis);
+        }
+        #endregion
+    }
+}
+
---
+++ CodeNameKidsNextDoor/Assets/Scripts/Stats.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace code.stats
+{
+    public class Stats
+    {
+        #region Vars
+        int[] stats = new int[6];
+        #endregion
+
+        #region Indexer
+        public int this[StatTypes type]
+        {
+            get { return stats[(int)type]; }
+            set { stats[(int)type] = value; }
+        }
+        #endregion
+
+        #region Methods
+        public Stats(int hp, int def, int str, int agl, int intellect, int wis)
+        {
+            this[StatTypes.HEALTH] = hp; this[StatTypes.DEFENSE] = def; this[StatTypes.STRENGTH] = str; this[StatTypes.AGILITY] = agl; this[StatTypes.INTELLECT] = intellect; this[StatTypes.WISDOM] = wis;
+        }
+
+        public void ModifyStat(StatTypes type, int modifier)
+        {
+            this[type] = this[type] + modifier;
+        }
+        #endregion
+
+        #region Stat Enums
+        public enum StatTypes { HEALTH, DEFENSE, STRENGTH, AGILITY, INTELLECT, WISDOM };
+        #endregion
+    }
+}
