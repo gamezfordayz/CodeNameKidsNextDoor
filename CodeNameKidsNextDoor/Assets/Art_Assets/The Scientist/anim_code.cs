19ac10eabdb31d9ef3dbc6ff161ffd6ef16e7471
wownecro <jesusmagallanez26@yahoo.com>
2016-01-19 21:10:33 -0800

    scientist_animation

---
+++ CodeNameKidsNextDoor/Assets/Art_Assets/The Scientist/anim_code.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anim_code : MonoBehaviour 
+{
+	Animator anim;
+	int rightHash = Animator.StringToHash("right")
+	int leftHash = Animator.StringToHash("left")
+	int upHash = Animator.StringToHash("up")
+	int downHash = Animator.StringToHash("down")
+
+	// Use this for initialization
+	void Start () 
+	{
+			anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (input.GetKey(KeyCode.D))
+			anim.SetFloat("speed",1);
+		else if(Input.GetKeyUp(KeyCode.D))
+			anim.SetFloat("speed",0);
+		if (input.GetKey(KeyCode.S))
+			anim.SetFloat("speed",1);
+		else if(Input.GetKeyUp(KeyCode.S))
+			anim.SetFloat("speed",0);
+		if (input.GetKey(KeyCode.A))
+			anim.SetFloat("speed",1);
+		else if(Input.GetKeyUp(KeyCode.A))
+			anim.SetFloat("speed",0);
+		if (input.GetKey(KeyCode.W))
+			anim.SetFloat("speed",1);
+		else if(Input.GetKeyUp(KeyCode.W))
+			anim.SetFloat("speed",0);
+	}
+}
