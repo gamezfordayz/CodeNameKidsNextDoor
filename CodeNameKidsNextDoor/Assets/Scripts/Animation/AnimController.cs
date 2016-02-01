3f8c065bdd30d812874aeccc8a8183690905cf5e
Wyatt Thornbury <w.m.thornbury@hotmail.com>
2016-01-31 18:42:09 -0800

    Fixed Animation

---
+++ CodeNameKidsNextDoor/Assets/Scripts/Animation/AnimController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimController : MonoBehaviour
+{
+    public float scaleX = .1f;
+    public GameObject[] skeletons = null;
+
+    void Start ()
+    {
+		scaleX = gameObject.transform.localScale.x;
+        ActivateSkele(skeletons[0]);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            ActivateSkele(skeletons[1]);
+            scaleX = Mathf.Abs(scaleX);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            ActivateSkele(skeletons[1]);
+            scaleX = -Mathf.Abs(scaleX);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            ActivateSkele(skeletons[0]);
+        }
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            ActivateSkele(skeletons[2]);
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void ActivateSkele(GameObject skele)
+    {
+        foreach (GameObject temp in skeletons)
+        {
+            if (temp == skele)
+            {
+                temp.SetActive(true);
+                continue;
+            }
+            temp.SetActive(false);
+        }
+    }
+
+}
---
+++ CodeNameKidsNextDoor/Assets/Scripts/Animation/anim_code.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anim_code : MonoBehaviour
+{
+	Animator anim;
+
+    void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetFloat("speed", 1);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetFloat("speed", 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetFloat("speed", 1);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetFloat("speed", 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetFloat("speed", 1);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            anim.SetFloat("speed", 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetFloat("speed", 1);
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetFloat("speed", 0);
+        }
+	}
+
+    
+}
